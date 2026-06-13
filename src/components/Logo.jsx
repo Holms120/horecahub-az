6a0edd354aeef387cf63e1f8daada4d471b62a14
Nuraddin Askerov <nuraddin314@gmail.com>
2026-06-13 20:17:01 +0400

    fix: Logo use logo-white.svg for light prop

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,13 +1,10 @@
 export default function Logo({ height = 38, light = false }) {
   return (
-    <img 
-      src="/logo.svg" 
-      alt="HorecaHub.az" 
-      height={height} 
-      style={{ 
-        height,
-        filter: light ? 'brightness(0) invert(1)' : 'none'
-      }} 
+    <img
+      src={light ? '/logo-white.svg' : '/logo.svg'}
+      alt="HorecaHub.az"
+      height={height}
+      style={{ height }}
     />
   )
 }
