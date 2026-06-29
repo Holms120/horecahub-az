795f7a36c8a7be247106caa0e4d1e06682a8fc9e
Nuraddin Askerov <nuraddin314@gmail.com>
2026-06-30 02:16:43 +0400

    feat: add Sentry error monitoring

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://6e3acc7df24799973cb7aa9be2ec2844@o4511651001335808.ingest.de.sentry.io/4511651014967376",
+});
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
